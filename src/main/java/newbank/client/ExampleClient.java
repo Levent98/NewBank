de42133a8febadccb20900e98a8cee9749010e11
Thomas Ashley <tta33@bath.ac.uk>
2026-03-19 22:10:53 +0000

    resolved merge conflict in ExampleClient

--- src/main/java/newbank/client/ExampleClient.java
+++ src/main/java/newbank/client/ExampleClient.java
@@ -13,65 +13,10 @@ public class ExampleClient {
     private PrintWriter bankServerOut;
     private BufferedReader serverIn;
 
-<<<<<<< HEAD
     public ExampleClient(String ip, int port) throws UnknownHostException, IOException {
         server = new Socket(ip, port);
         bankServerOut = new PrintWriter(server.getOutputStream(), true);
         serverIn = new BufferedReader(new InputStreamReader(server.getInputStream()));
-=======
-  public ExampleClient(String ip, int port) throws UnknownHostException, IOException {
-    server = new Socket(ip,port);
-    userInput = new BufferedReader(new InputStreamReader(System.in)); 
-    // server.getOutputStream() gives you an output stream to the server
-    // PrintWriter(server.getOutputStream(), true) wraps this stream to send lines of text (with auto-flush)
-    bankServerOut = new PrintWriter(server.getOutputStream(), true); 
-
-    bankServerResponseThread = new Thread() {
-      private BufferedReader bankServerIn = new BufferedReader(new InputStreamReader(server.getInputStream())); 
-
-      @Override
-      public void run() {
-        try {
-          while(true) {
-            String response = bankServerIn.readLine();
-            // US01 Added to prevent a wall of "null" on the client
-            if (response == null) {
-              System.out.println("Disconnected from server, please reconnect or hit ctrl-D");
-              break;
-            }
-            System.out.println(response);
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
-          return;
-        }
-      }
-    };
-    bankServerResponseThread.start();
-  }
-
-  @Override
-  public void run() {
-    // restart the input loop if an exception occurs,
-    // but usually, if userInput.readLine() fails, the program is not recoverable.
-outer: // this is to modify the original code as little as possible but the while loops should be simplified
-    while(true) {
-      try {
-        while(true) {
-          String command = userInput.readLine();
-          // US01 Added to be able to close the client with ctrl-D
-          if (command == null) {
-            System.out.println("Shutting down... bye!");
-            break outer;
-          }
-          bankServerOut.println(command);
-        }				
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-        // no return stmt because the outer while restarts the thread if needed
-      }
->>>>>>> origin/feature/UserStory01_SetPassword_TS
     }
 
     public void sendCommand(String command) {
