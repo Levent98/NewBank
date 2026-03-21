ba3b0b07a7f957edf47bb9e9f0625d3897376ebd
Levent Keskin <llk37@bath.ac.uk>
2026-03-21 21:57:48 +0200

    Revert "Feature/user story03 create new account rl"

--- src/main/java/newbank/server/Customer.java
+++ src/main/java/newbank/server/Customer.java
@@ -21,18 +21,4 @@ public class Customer {
   public void addAccount(Account account) {
     accounts.add(account);    
   }
-  // Adds the new account as long as there are no more than 10 accounts already setup
-  public boolean addAccount(String name) {
-    for (Account a : accounts) {
-      if (a.getName().equalsIgnoreCase(name)) {
-        return false;
-      }
-      if (accounts.size()>10) {
-        return false;
-      }
-    }
-  // Default opening balance set to 0.0
-  accounts.add(new Account(name, 0.0));
-    return true;
-  }
 }
