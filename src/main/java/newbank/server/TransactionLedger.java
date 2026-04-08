31fb88e8be173a74a12e574084e81d8ab9d295b2
Thomas Ashley <tta33@bath.ac.uk>
2026-04-09 00:34:56 +0100

    view all + ledger solution workspace

---
+++ src/main/java/newbank/server/TransactionLedger.java
@@ -0,0 +1,12 @@
+package newbank.server;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class TransactionLedger {
+
+   // ledger hashmap storage transaction manager to write transactions to ledger
+    
+}
